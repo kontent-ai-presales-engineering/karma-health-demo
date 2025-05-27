21583eae48e9a6d68fc409417554c47f8d7040af
Maarten van den Hooven <maartenv@kontent.ai>
2025-05-27 17:10:26 +0200

    remove condition

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -20,7 +20,7 @@ const useLandingPage = (isPreview: boolean, lang: string | null) => {
   const [landingPage, setLandingPage] = useState<Replace<LandingPage, { elements: Partial<LandingPage["elements"]> }> | null>(null);
 
   const handleLiveUpdate = useCallback((data: IUpdateMessageData) => {
-    if (landingPage && data.item.codename === landingPage.system.codename) {
+    if (landingPage) {
       // Use applyUpdateOnItemAndLoadLinkedItems to ensure all linked content is updated
       applyUpdateOnItemAndLoadLinkedItems(
         landingPage,
